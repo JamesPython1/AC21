87455af17d7a1346254a60721bb83f332317c17f
jgrac <20861@oakham.rutland.sch.uk>
2021-12-03 06:56:08 +0000

    Completed part 1 day 3

--- 3/part1.py
+++ 3/part1.py
@@ -1,2 +1,33 @@
 
 data = open("input.txt", "r").read().splitlines()
+bits = [[0, 0] for i in range(len(data[0]))]
+print(bits)
+
+for line in data:
+    for i in range(len(line)):
+        if int(line[i]) == 1: bits[i][1] += 1
+        if int(line[i]) == 0: bits[i][0] += 1
+
+# gamma (most common) and epsilon (least common)
+gamma = ""
+
+print(bits)
+for i in range(len(bits)):
+    if bits[i][0] > bits[i][1]: gamma += "0"
+    else:
+        gamma += "1"
+
+print(gamma)
+
+epsilon = ""
+for i in range(len(bits)):
+    if bits[i][0] < bits[i][1]: epsilon += "0"
+    else:
+        epsilon += "1"
+
+print(epsilon)
+
+gammaDenary = int(gamma, 2)
+epsilonDenary = int(epsilon, 2)
+powerConsumption = gammaDenary * epsilonDenary
+print(powerConsumption)
