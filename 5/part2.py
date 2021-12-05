96c18ea48b726738ce788ad5aeed9d2ba6c597fe
jgrac <20861@oakham.rutland.sch.uk>
2021-12-05 16:38:19 +0000

    Day 5, part 2 done

---
+++ 5/part2.py
@@ -0,0 +1,40 @@
+
+coordData = open("input.txt", "r").read().splitlines()
+startCoords = [c.split(" -> ")[0].split(",") for c in coordData]
+endCoords = [c.split(" -> ")[1].split(",") for c in coordData]
+coordMap = {}
+
+for it in range(len(startCoords)):
+    x1, x2 = int(startCoords[it][0]), int(endCoords[it][0])
+    y1, y2 = int(startCoords[it][1]), int(endCoords[it][1])
+    coords = []
+    if x1 == x2:
+        for i in range(abs(y2-y1)+1):
+            if y1 > y2: coords.append((x1,y1-i))
+            else: coords.append((x1,y1+i))
+    elif y1 == y2:
+        for i in range(abs(x2-x1)+1):
+            if x1 > x2: coords.append((x1-i,y1))
+            else: coords.append((x1+i,y1))
+    else:
+        print((x1,y1),(x2,y2))
+        xs, ys = [], []
+        for i in range(abs(x2 - x1) + 1):
+            if x1 > x2: xs.append(x1 - i)
+            else: xs.append(x1 + i)
+        for i in range(abs(y2 - y1) + 1):
+            if y1 > y2: ys.append(y1 - i)
+            else: ys.append(y1 + i)
+        for i in range(len(xs)): coords.append((xs[i],ys[i]))
+
+
+    for coord in coords:
+        if coord not in list(coordMap.keys()): coordMap[coord] = 1
+        else: coordMap[coord] += 1
+
+count = list(coordMap.values())
+numberOfPlusTwo = 0
+for c in count:
+    if c >= 2: numberOfPlusTwo += 1
+
+print(numberOfPlusTwo)
